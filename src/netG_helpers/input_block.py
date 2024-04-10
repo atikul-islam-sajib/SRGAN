934ee2aa48a3442abce0f33164d7d3df30dfc7fd
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-04-11 01:32:23 +0200

    doc strings is added in the input block

--- src/netG_helpers/input_block.py
+++ src/netG_helpers/input_block.py
@@ -10,7 +10,35 @@ from utils import params
 
 
 class InputBlock(nn.Module):
+    """
+    A module representing the input block of a neural network generator (netG).
+    
+    This block initializes with the input and output channels and sets up a convolutional layer 
+    followed by a PReLU activation layer based on parameters specified in the `params` function.
+    
+    Attributes:
+        in_channels (int): The number of input channels.
+        out_channels (int): The number of output channels.
+        kernel_size (int): Kernel size for the convolutional layer.
+        stride (int): Stride for the convolutional layer.
+        padding (int): Padding for the convolutional layer.
+        model (nn.Sequential): Sequential model comprising the convolutional and PReLU layers.
+    
+    Examples:
+        >>> input_block = InputBlock(in_channels=3, out_channels=64)
+        >>> images = torch.randn(1, 3, 64, 64)
+        >>> output = input_block(images)
+        >>> print(output.size())
+        torch.Size([1, 64, 64, 64])
+    """
     def __init__(self, in_channels=None, out_channels=None):
+        """
+        Initializes the input block with the given input and output channels.
+        
+        Parameters:
+            in_channels (int, optional): The number of channels in the input image. Defaults to None.
+            out_channels (int, optional): The number of channels produced by the convolution. Defaults to None.
+        """
         super(InputBlock, self).__init__()
 
         self.in_channels = in_channels
@@ -26,6 +54,14 @@ class InputBlock(nn.Module):
             print("Input block not implemented")
 
     def input_block(self):
+        """
+        Defines the layers of the input block.
+        
+        This method constructs an OrderedDict of layers comprising a convolutional layer followed by a PReLU activation layer.
+        
+        Returns:
+            nn.Sequential: A sequential container of the layers defined.
+        """
         layers = OrderedDict()
 
         layers["conv"] = nn.Conv2d(
@@ -40,6 +76,18 @@ class InputBlock(nn.Module):
         return nn.Sequential(layers)
 
     def forward(self, x=None):
+        """
+        Defines the forward pass of the input block.
+        
+        Parameters:
+            x (torch.Tensor, optional): The input tensor. Defaults to None.
+        
+        Raises:
+            Exception: If the input tensor x is None.
+        
+        Returns:
+            torch.Tensor: The output tensor after passing through the model.
+        """
         if x is not None:
             return self.model(x)
         else:
