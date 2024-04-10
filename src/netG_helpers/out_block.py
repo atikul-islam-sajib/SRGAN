60b4ef6d5269190664dd0f724613b5a2073b6157
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-04-11 01:36:25 +0200

    doc strings is added in the out block

--- src/netG_helpers/out_block.py
+++ src/netG_helpers/out_block.py
@@ -10,7 +10,35 @@ from utils import params
 
 
 class OutputBlock(nn.Module):
+    """
+    Defines the output block for a neural network generator (netG), applying a convolutional layer followed by
+    a Tanh activation to generate the final output. This block is designed to convert the feature maps from
+    the preceding layers to the desired number of output channels, typically corresponding to the image's
+    color channels.
+
+    Attributes:
+        in_channels (int): Number of input channels to the block.
+        out_channels (int): Desired number of output channels.
+        kernel_size (int): Kernel size for the convolutional layer.
+        stride (int): Stride for the convolutional layer.
+        padding (int): Padding for the convolutional layer.
+        model (nn.Sequential): Sequential model containing the convolutional and Tanh activation layers.
+
+    Examples:
+        >>> output_block = OutputBlock(in_channels=64, out_channels=3)
+        >>> images = torch.randn(1, 64, 256, 256)
+        >>> output = output_block(images)
+        >>> print(output.size())
+        torch.Size([1, 3, 256, 256])
+    """
     def __init__(self, in_channels=None, out_channels=None):
+        """
+        Initializes the output block with specified input and output channels, constructing the model architecture.
+
+        Parameters:
+            in_channels (int, optional): The number of channels in the input tensor. Defaults to None.
+            out_channels (int, optional): The number of channels in the output tensor. Defaults to None.
+        """
         super(OutputBlock, self).__init__()
 
         self.in_channels = in_channels
@@ -25,6 +53,12 @@ class OutputBlock(nn.Module):
             print("Output block not implemented".capitalize())
 
     def output_block(self):
+        """
+        Constructs the output block's layers, including a convolutional layer followed by a Tanh activation layer.
+        
+        Returns:
+            nn.Sequential: The sequential container holding the defined layers.
+        """
         layers = OrderedDict()
 
         layers["conv"] = nn.Conv2d(
@@ -39,6 +73,18 @@ class OutputBlock(nn.Module):
         return nn.Sequential(layers)
 
     def forward(self, x):
+        """
+        Forward pass through the output block.
+
+        Parameters:
+            x (torch.Tensor): The input tensor to the block.
+
+        Raises:
+            Exception: If the input tensor x is None, indicating incomplete implementation.
+        
+        Returns:
+            torch.Tensor: The output tensor after processing by the block.
+        """
         if x is not None:
             return self.model(x)
         else:
