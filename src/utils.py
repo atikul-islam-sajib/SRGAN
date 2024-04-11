26eca9c008f59f4a40558f1abea96351377edafb
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-04-11 16:39:58 +0200

    utils file is updated

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,20 @@
+import os
+import joblib
 import yaml
+
+
+def dump(value, filename):
+    if (value is not None) and (filename is not None):
+        joblib.dump(value=value, filename=filename)
+    else:
+        raise Exception("Value or filename not provided")
+
+
+def load(filename):
+    if filename is not None:
+        return joblib.load(filename=filename)
+    else:
+        raise Exception("Filename not provided")
 
 
 def params():
