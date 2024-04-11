b15444a8643f69420d0b2b096077fbb2a606217a
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-04-11 03:40:59 +0200

    features block is added in the src - netD

--- src/netD_helpers/features_block.py
+++ src/netD_helpers/features_block.py
@@ -7,38 +7,32 @@ sys.path.append("src/")
 
 from utils import params
 
-class InputBlock(nn.Module):
-    def __init__(self, in_channels = None, out_channels = None):
-        super(InputBlock, self).__init__()
+class FeatureBlock(nn.Module):
+    def __init__(self, in_channels = None, out_channels = None, kernel_size=3, stride=2, padding=1):
+        super(FeatureBlock, self).__init__()
         
         self.in_channels = in_channels
         self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
         
-        self.kernel_size = params()["netD"]["kernel_size"]
-        self.stride = params()["netD"]["stride"]
-        self.padding = params()["netD"]["padding"]
+        self.model = self.feature_block()
         
-        self.model = self.input_block()
-        
-    def input_block(self):
+    def feature_block(self):
         return nn.Sequential(
             nn.Conv2d(self.in_channels, self.out_channels, self.kernel_size, self.stride, self.padding),
-            nn.LeakyReLU(negative_slope=0.2, inplace=True),
-            nn.Conv2d(self.out_channels, self.out_channels, self.kernel_size, self.stride, self.padding),
             nn.BatchNorm2d(num_features=self.out_channels),
             nn.LeakyReLU(negative_slope=0.2, inplace=True)
-        )
+        )       
         
     def forward(self, x):
         if x is not None:
             return self.model(x)
         
-        else:
-            raise Exception("Input channels and output channels are required".capitalize())
         
-
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Input block for netD".title())
+    parser = argparse.ArgumentParser(description="Features block for netD".title())
 
     parser.add_argument("--in_channels", type=int, default=1, help="Input channels")
     parser.add_argument("--out_channels", type=int, default=64, help="Output channels")
@@ -46,13 +40,13 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.in_channels and args.out_channels:
-        input_block = InputBlock(
+        features_block = FeatureBlock(
             in_channels=args.in_channels, out_channels=args.out_channels
         )
 
-        images = torch.randn(1, 3, 64, 64)
+        images = torch.randn(1, 64, 64, 64)
 
-        print(input_block(images).size())
+        print(features_block(images).size())
 
     else:
-        raise Exception("Input channels and output channels are required".capitalize())
+        raise Exception("Features Block channels and output channels are required".capitalize())
