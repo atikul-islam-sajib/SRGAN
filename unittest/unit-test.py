8cd5d3073aada2d10d4df62182f8284e465e841b
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-04-12 19:32:17 +0200

    netD is passed the unittest

---
+++ unittest/unit-test.py
@@ -0,0 +1,26 @@
+import sys
+import unittest
+import torch
+
+sys.path.append("src/")
+
+from generator import Generator
+from discriminator import Discriminator
+
+
+class UnitTest(unittest.TestCase):
+    def setUp(self):
+        self.images = torch.randn(1, 3, 64, 64)
+        self.netG = Generator(in_channels=3, out_channels=64)
+
+    def tearDown(self) -> None:
+        return super().tearDown()
+
+    def test_netG_shape(self):
+        self.assertEqual(
+            self.netG(self.images).size(), torch.Size([1, 3, 64 * 4, 64 * 4])
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
