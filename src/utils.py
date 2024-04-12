6182e09ed8392e3c07f654f13fe67b0e6fc454bf
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-04-12 20:44:50 +0200

    utils file is added with weight init

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,8 @@
 import os
 import joblib
 import yaml
+import torch
+import torch.nn as nn
 
 
 def dump(value, filename):
@@ -20,3 +22,14 @@ def load(filename):
 def params():
     with open("./params.yml", "r") as file:
         return yaml.safe_load(file)
+
+
+def weight_int(m):
+    classname = m.__class__.__name__
+
+    if classname.find("Conv") != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+
+    elif classname.find("BatchNorm") != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
